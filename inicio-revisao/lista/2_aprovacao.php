c6eaf876c0d28598a08deca4bb9aedd2e36e8c65
Miguel-Klem <miguelbklem10@gmail.com>
2026-08-17 14:50:53 -0300

    Correção do <br>

--- inicio-revisao/lista/2_aprovacao.php
+++ inicio-revisao/lista/2_aprovacao.php
@@ -6,9 +6,9 @@ $faltas = 10;
 
 // O && faz com que ambas das variáveis sejam lidas e confirmadas
 if ($media >= 6.0 && $faltas <= 15) {
-    echo "Aluno APROVADO! Média: $media | Faltas: $faltas";
+    echo "Aluno APROVADO! <br> Média: $media <br> Faltas: $faltas";
 } else {
-    echo "Aluno REPROVADO! Média: $media | Faltas: $faltas";
+    echo "Aluno REPROVADO! <br> Média: $media <br> Faltas: $faltas";
 }
 
 ?>
