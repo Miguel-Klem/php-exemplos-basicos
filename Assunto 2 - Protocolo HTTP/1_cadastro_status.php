14cdb7800944a67534f4746efad751ab8d3943c3
Miguel-Klem <miguelbklem10@gmail.com>
2026-08-18 13:45:35 -0300

    Atividade 2

---
+++ Assunto 2 - Protocolo HTTP/1_cadastro_status.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Cadastro - Status Codes </title>
+
+</head>
+<body>
+
+    <h1> Cadastro de Alunos (Com status Codes) </h1>
+
+    <form method="post" action="">
+
+        <label for="nome"> Nome: </label>
+        <input type="text" name="nome" id="nome" required><br><br>
+
+        <label for="idade"> Idade: </label>
+        <input type="number" name="idade" id="idade" required><br><br>
+
+        <button type="submit"> Enviar </button>
+
+    </form>
+
+</body>
+</html>
